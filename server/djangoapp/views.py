fbe6b7eaad373d639230ff900c39b489b4299bf4
neelperumandla <neelp006@@gmail.com>
2025-07-26 16:45:09 -0400

    made fixes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,6 +13,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 
 # Get an instance of a logger
@@ -47,7 +48,44 @@ def logout_request(request):
 # @csrf_exempt
 # def registration(request):
 # ...
+@csrf_exempt
+def registration(request):
+    # context = {}
+    # Load JSON data from the request body
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+    username_exist = False
+    # email_exist = False
+    try:
+        # Check if user already exists
+        User.objects.get(username=username)
+        username_exist = True
+    except Exception:
+        # If not, simply log this is a new user
+        logger.debug("{} is new user".format(username))
 
+    # If it is a new user
+    if not username_exist:
+        # Create user in auth_user table
+        user = User.objects.create_user(
+           username=username,
+           first_name=first_name,
+           last_name=last_name,
+           password=password,
+           email=email,
+        )
+
+        # Login the user and redirect to list page
+        login(request, user)
+        data = {"userName": username, "status": "Authenticated"}
+        return JsonResponse(data)
+    else:
+        data = {"userName": username, "error": "Already Registered"}
+        return JsonResponse(data)
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
